3a82cdbe7c306e025d9cfded7bf834566da10dcd
Nadzirah Abd Rahman <nadzirahabdrahman@hotmail.com>
2023-12-21 01:08:35 +0800

    react-handling array

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  // let students = ["Jett", "Reyna", "Raze", "Neon"];
+
+  function student(name, age, hostel, hostelRoom, city) {
+    // 'this' is when use this function it will refer to 
+    this.name = name;
+    this.age = age;
+    this.hostel = hostel;
+    this.hostelRoom = hostelRoom;
+    this.city = city;
+  }
+
+  let student1 = new student("Haziq", 23, true, 12, "Melaka");
+  let student2 = new student("Nadzirah", 25, false, 0, "Pahang");
+
+  let students = [student1, student2];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {/* {students.map((val, key) => { */}
+        {/* return ( */}
+          {/* // key = index in the array
+          // must have parents element div */}
+          {/* <div key={key}> */}
+            {/* val = value in the array */}
+            {/* <h1>{val}</h1> */}
+          {/* </div> */}
+        {/* );
+      })} */}
+
+      {
+        students.map((student, key) => {
+          return (
+            <div key={key} className='card'>
+              <p>Name:{student.name}</p>
+              <p>Age:{student.age}</p>
+              <p>Live in hostel:{student.hostel ? "Yes" : "No"}</p>
+              {student.hostel &&  <p>Room number:{student.hostelRoom}</p>}
+              <p>City:{student.city}</p>
+            </div>
+          )
+        })
+      }
+    </>
+  )
+  
 }
 
 export default App;
